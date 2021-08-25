b962702bcca9a5d9c9a879d6ee408a62c3c2516c
Eduardo <dreamsjumperh9c@gmail.com>
2021-08-24 20:09:44 -0500

    cors enabled

--- index.js
+++ index.js
@@ -3,11 +3,14 @@ const bodyParser = require('body-parser');
 const connectDatabase = require('./database/connectDatabase');
 const express = require('express');
 const fileUpload = require('express-fileupload');
+const cors = require('cors');
 const routes = require('./routes');
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+
+app.use(cors())
 
 app.use(fileUpload());
 
